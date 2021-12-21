4112329277cec93bbb8ec8062be5548332410b2d
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2021-12-21 19:02:29 +0200

    Connect the UI to the backend and add initial functionallity

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ballImages = [
+        UIImage(named: "ball1"),
+        UIImage(named: "ball2"),
+        UIImage(named: "ball3"),
+        UIImage(named: "ball4"),
+        UIImage(named: "ball5")
+    ]
+    
+    @IBOutlet weak var ballImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askButtonAction(_ sender: UIButton) {
+        self.answerQuestion()
+    }
+    
+    func answerQuestion() {
+        UINotificationFeedbackGenerator().notificationOccurred(.success)
+        self.ballImageView.image = ballImages.randomElement()!
+    }
+    
 }
 
