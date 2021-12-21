47b3909f83c723a83bd6852b9f0f790111486d74
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2021-12-21 18:37:03 +0200

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Kostadin Samardzhiev on 21.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
